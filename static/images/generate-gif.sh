52c0fd2c28357eca8258e637804fb5fe5b97ab8f
Lucas Aleixo <aleixo2lucas@gmail.com>
2023-03-22 03:09:15 -0300

    some updates

--- static/images/generate-gif.sh
+++ static/images/generate-gif.sh
@@ -14,5 +14,8 @@ ffmpeg \
  [2]format=rgba,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+8/TB[f1]; \
  [3]format=rgba,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+12/TB[f2]; \
  [4]format=rgba,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+16/TB[f3]; \
+ [5]format=rgba,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+20/TB[f4]; \
+ [6]format=rgba,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+24/TB[f5]; \
+ [7]format=rgba,fade=d=1:t=in:alpha=1,setpts=PTS-STARTPTS+28/TB[f6]; \
  [0][f0]overlay[bg1];[bg1][f1]overlay[bg2];[bg2][f2]overlay[bg3];[bg3][f3]overlay,split[v0][v1]; \
  [v0]palettegen[p];[v1][p]paletteuse[v]" -map "[v]" images.gif
