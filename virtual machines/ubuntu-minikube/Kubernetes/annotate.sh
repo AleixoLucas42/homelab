3cb8db56cb478570340c4817efaf61514fc596f4
Lucas Aleixo <aleixo2lucas@gmail.com>
2023-04-11 20:38:01 -0300

    Adicionado anotação para limitar o numero de conexões por cliente pra 1

---
+++ virtual machines/ubuntu-minikube/Kubernetes/annotate.sh
@@ -0,0 +1,8 @@
+!#/bin/bash
+
+kubectl get ingress -A | awk -F' ' '{print $2" "$1}' | grep -v "NAME" | while read line
+do
+    resource=$(echo $line | cut -d" " -f1)
+    ns=$(echo $line | cut -d" " -f2)
+    kubectl annotate -n ${ns} ingress/${resource} "nginx.ingress.kubernetes.io/limit-connections=1"
+done
