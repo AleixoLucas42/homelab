49d9754dac4f3aea80b8f8f48e87d7e3b0226cf8
Lucas Aleixo da Silva <lucas.silva@2rpnet.com>
2024-07-07 22:07:07 -0300

    Atualizando a documentação

--- virtual machines/ubuntu-minikube/Kubernetes/annotate.sh
+++
@@ -1,9 +0,0 @@
-!#/bin/bash
-
-kubectl get ingress -A | awk -F' ' '{print $2" "$1}' | grep -v "NAME" | while read line
-do
-    resource=$(echo $line | cut -d" " -f1)
-    ns=$(echo $line | cut -d" " -f2)
-    #kubectl annotate -n ${ns} ingress/${resource} "nginx.ingress.kubernetes.io/limit-connections=5"
-    kubectl annotate -n ${ns} ingress/${resource} nginx.ingress.kubernetes.io/limit-connections-
-done
--- virtual machines/ubuntu-minikube/vault-backup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export $(cat /home/aleixohome/.env | xargs)
-python3 vault-backup.py
