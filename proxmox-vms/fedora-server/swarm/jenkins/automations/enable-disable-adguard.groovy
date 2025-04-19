6a65b67d5f9e4803aba9b941d6e30045cee0ed95
Lucas Aleixo <lucas.silva@2rpnet.com>
2025-04-19 10:12:06 -0300

    wip

--- proxmox-vms/fedora-server/swarm/jenkins/automations/enable-disable-adguard.groovy
+++ proxmox-vms/fedora-server/swarm/jenkins/automations/enable-disable-adguard.groovy
@@ -6,13 +6,32 @@ pipeline {
         ADGUARD_PASSWORD = credentials('ADGUARD_PASSWORD')
     }
     stages {
-        stage('run_script') {
+        stage('prepare_environment') {
             steps {
                 script {
                     def buildScript = '''#!/bin/bash
-                    script_url="https://raw.githubusercontent.com/AleixoLucas42/homelab/refs/heads/main/proxmox-vms/fedora-server/swarm/jenkins/automations/enable-disable-adguard.sh"
+                    auth_header=$(echo -n "${ADGUARD_USER}:${ADGUARD_PASSWORD}" | base64)
+                    status=$(curl -sk --location --request GET "http://adguardhome_adguardhome/control/status" \
+                        --header "Accept: application/json" \
+                        --header "Content-Type: application/json" \
+                        --header "Authorization: Basic $auth_header" \
+                        | grep -o '"protection_enabled":[^,]*' | cut -d: -f2 | tr -d ' ')
 
-                    curl -sSL "$script_url" | bash
+                    echo "Proteção está atualmente: $status"
+
+                    if [ "$status" == "true" ]; then
+                        new_status=false
+                    else
+                        new_status=true
+                    fi
+
+                    curl -sk --location --request POST "http://adguardhome_adguardhome/control/protection" \
+                        --header "Accept: application/json" \
+                        --header "Content-Type: application/json" \
+                        --header "Authorization: Basic $auth_header" \
+                        --data "{\"enabled\":$new_status}"
+
+                    echo -e "\nNovo status enviado: enabled = $new_status"
                     '''
                     sh buildScript
                 }
